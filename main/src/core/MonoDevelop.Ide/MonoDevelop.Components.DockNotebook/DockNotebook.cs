257c2b26374f53a37222224a2fe96d9d91e0f836
Jose Medrano <jose.medrano@microsoft.com>
2018-02-06 21:04:26 +0100

    Reverts changes in UpdatedTabWidth

--- main/src/core/MonoDevelop.Ide/MonoDevelop.Components.DockNotebook/DockNotebook.cs
+++ main/src/core/MonoDevelop.Ide/MonoDevelop.Components.DockNotebook/DockNotebook.cs
@@ -137,14 +137,6 @@ namespace MonoDevelop.Components.DockNotebook
 
 		Cursor fleurCursor = new Cursor (CursorType.Fleur);
 
-		internal bool ContainsTab (DockNotebookTab tab)
-		{
-			if (AllTabs.Any (t => tab == t)) {
-				return true;
-			}
-			return false;
-		}
-
 		public event TabsReorderedHandler TabsReordered;
 		public event EventHandler<TabEventArgs> TabClosed;
 		public event EventHandler<TabEventArgs> TabActivated;
@@ -479,10 +471,11 @@ namespace MonoDevelop.Components.DockNotebook
 			base.OnDestroyed ();
 		}
 
-		List<DockNotebookTab> GetCollectionForTab (DockNotebookTab tab)
-		{
-			return tab.IsPreview ? previewPages : pages;
-		}
+		internal bool ContainsTab (DockNotebookTab tab) => GetReadOnlyCollectionForTab (tab).Contains (tab);
+
+		List<DockNotebookTab> GetCollectionForTab (DockNotebookTab tab) => tab.IsPreview ? previewPages : pages;
+
+		public ReadOnlyCollection<DockNotebookTab> GetReadOnlyCollectionForTab (DockNotebookTab tab) => tab.IsPreview ? PreviewTabs : NormalTabs;
 	}
 
 	class DockNotebookChangedArgs : EventArgs
